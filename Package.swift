64a1b6d15aa181e90c73a45ff439d2c4af856664
Carlos García <carlosypunto@gmail.com>
2019-11-16 04:14:45 +0100

    add platforms to pachage

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "Bagel",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         .library(name: "Bagel", targets: ["Bagel"])
     ],
