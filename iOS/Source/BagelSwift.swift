a8025fab33877f4929473e60300000ef26f429ff
Carlos García <carlosypunto@gmail.com>
2019-11-16 05:15:23 +0100

    add swift class

---
+++ iOS/Source/BagelSwift.swift
@@ -0,0 +1,15 @@
+//
+//  BagelSwift.swift
+//  Bagel
+//
+//  Created by Carlos on 16/11/2019.
+//  Copyright © 2019 Yagiz Lab. All rights reserved.
+//
+
+class BagelSwift {
+    
+    static func start() {
+        Bagel.start()
+    }
+    
+}
